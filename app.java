ddca8e71a32edc0f1b36900e027de509294b1bf5
PoornaPrasadRoyal <poorna7989@gmail.com>
2025-11-19 13:55:12 +0530

    Update app.java

--- app.java
+++ app.java
@@ -4,3 +4,17 @@ public class HelloWorld {
     }
 }
 my first push from local to global
+
+    //new code added
+    class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
